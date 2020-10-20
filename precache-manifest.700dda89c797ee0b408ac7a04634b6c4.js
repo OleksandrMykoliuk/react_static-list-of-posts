9eb335a3285d28b5ca7b7f0bb9d02b8241748e8f
OleksandrMykoliuk <69759907+OleksandrMykoliuk@users.noreply.github.com>
2020-10-20 17:49:21 +0300

    Updates

---
+++ precache-manifest.700dda89c797ee0b408ac7a04634b6c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f90923b4d99f428f1e9b00664938c1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0705ddd18b36c6d22a62",
+    "url": "./static/css/main.43d858fc.chunk.css"
+  },
+  {
+    "revision": "d36cf7d1584e4e69c9e6",
+    "url": "./static/js/2.0472d363.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0472d363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0705ddd18b36c6d22a62",
+    "url": "./static/js/main.144a26db.chunk.js"
+  },
+  {
+    "revision": "4d809184eca59fc21746",
+    "url": "./static/js/runtime-main.74611449.js"
+  }
+]);
